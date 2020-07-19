358fc311b36fad8a34ef8608f288d05830205455
Jay Naporski <Jaytanica777@gmail.com>
2020-07-19 10:36:56 -0500

    upload from desktop

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+  {
+    name:"Thomas",
+    photo:"https://vignette.wikia.nocookie.net/ttte/images/d/d6/MainThomasCGI2.png/revision/latest/scale-to-width-down/1000?cb=20200624053802",
+    scores: [
+          "5",
+          "2",
+          "4",
+          "2",
+          "2",  
+          "1",
+          "5",
+          "4",
+          "5",
+          "4"
+        ]
+  },
+  {
+  name:"Waluigi",
+  photo:"https://en.wikipedia.org/wiki/Waluigi#/media/File:Waluigi.png",
+  scores: [
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",  
+        "2",
+        "4",
+        "2",
+        "3",
+        "2"
+      ]
+},
+{
+name:"Gromit Mug",
+photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLZDYaC3AWvjzrYizxddzxxUSrS9WrchvmwQ&usqp=CAU",
+  scores: [
+      "1",
+      "5",
+      "1",
+      "5",
+      "1",  
+      "5",
+      "1",
+      "1",
+      "1",
+      "5"
+  ]
+},
+{
+  name:"Slash",
+  photo:"https://www.rollingstone.com/wp-content/uploads/2018/08/1319_FOB_Mix_QnA_Slash_A.jpg",
+  scores: [
+        "5",
+        "3",
+        "5",
+        "1",
+        "4",  
+        "1",
+        "2",
+        "1",
+        "5",
+        "5"
+      ]
+  },
+  {
+    name:"Ryu",
+    photo:"https://vignette.wikia.nocookie.net/streetfighter/images/4/46/Ryurender.png/revision/latest?cb=20170728171704",
+    scores: [
+          "5",
+          "5",
+          "4",
+          "4",
+          "2",  
+          "2",
+          "5",
+          "4",
+          "4",
+          "5"
+        ]
+    }
+];
+  
+module.exports = friends;
---
+++ readme.md
@@ -0,0 +1,1 @@
+An app made for a fullstack boot camp. it has a front end and back end that matches you with an array of friends based on a personality test!
